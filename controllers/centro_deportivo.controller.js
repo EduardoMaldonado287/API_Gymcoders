e6b97cd290f4e1643989c2e50a8246ac2734f2fe
EduardoMaldonado287 <a00832361@itesm.mx>
2023-04-24 11:09:11 -0600

    Centro Deportivo CRUD

--- controllers/centro_deportivo.controller.js
+++ controllers/centro_deportivo.controller.js
@@ -1,15 +1,77 @@
 const centroDeportivoRoute = require('express').Router();
-const {v4: uuidv4} = require('uuid');
 const centroDeportivoModel = require('../models/centro_deportivo.model');
+const bodyParser = require('body-parser');
 
+centroDeportivoRoute.use(bodyParser.urlencoded({ extended: true }));
+centroDeportivoRoute.use(bodyParser.json());
 
 centroDeportivoRoute.get('/', async(req, res) => {
-    centroDeportivoModel.allTodo()
+    centroDeportivoModel.allCentroDeportivo()
     .then(data => {
         res.status(200).json({ data });
     })
     .catch(error => {
         res.status(500).json({ error });
+    });
+});
+
+centroDeportivoRoute.delete('/:id', async (req, res) => {
+    const {id: id_centro_deportivo} = req.params;
+    centroDeportivoModel.deleteCentroDeportivo(id_centro_deportivo)
+    .then((rowCount, more) => {
+        console.log("Item deleted succesfully");
+        res.json("Item deleted succesefully");
+        res.status(200).json({ rowCount, more });
+    })
+    .catch(error => {
+        res.status(500).json({ error });
+    })
+});
+
+centroDeportivoRoute.post('/', async (req, res) => {
+    const {
+        id_centro_deportivo,
+        nombre,
+    } = req.body;
+    centroDeportivoModel.addCentroDeportivo({
+        id_centro_deportivo,
+        nombre,
+    })
+    .then((rowCount, more) => {
+        res.status(200).json(
+            {
+            data: {
+                rowCount,
+                more,
+                id_centro_deportivo,
+            } 
+        });
+    })
+    .catch(error => {
+        res.status(500).json({error});
+    });
+});
+
+centroDeportivoRoute.put('/:id', async (req, res) => {
+    const {id: id_centro_deportivo} = req.params;
+    const {
+        nombre,
+    } = req.body;
+    centroDeportivoModel.updateCentroDeportivo({
+        id_centro_deportivo,
+        nombre,
+    })
+    .then((rowCount, more) => {
+        res.status(200).json({
+            data: {
+                rowCount,
+                more,
+                id_centro_deportivo,
+            },
+        });
+    })
+    .catch(error => {
+        res.status(500).json({error});
     });
 });
 
--- models/centro_deportivo.model.js
+++ models/centro_deportivo.model.js
@@ -1,13 +1,62 @@
 const execQuery = require('../helpers/execQuery');
 const TYPES = require('tedious').TYPES;
 
-const allTodo = () => {
+const allCentroDeportivo = () => {
     const query = `
         SELECT * FROM [dbo].[centro_deportivo]
     `;
     return execQuery.execReadCommand(query);
 };
 
+const deleteCentroDeportivo = (id_centro_deportivo) => {
+    const query = `
+        DELETE FROM [dbo].[centro_deportivo]
+        WHERE id_centro_deportivo = @id_centro_deportivo
+    `;
+    const parameters = [
+        {name: 'id_centro_deportivo', type: TYPES.Int, value: id_centro_deportivo}
+    ];
+    return execQuery.execWriteCommand(query, parameters);
+};
+
+const addCentroDeportivo = (centroDeportivoData) => {
+    const {
+        id_centro_deportivo,
+        nombre,
+    } = centroDeportivoData;
+
+    const query = `
+        INSERT INTO [dbo].[centro_deportivo] (id_centro_deportivo, nombre)
+        VALUES (@id_centro_deportivo, @nombre)
+    `;
+    const parameters = [
+        {name: 'id_centro_deportivo', type: TYPES.Int, value: id_centro_deportivo},
+        {name: 'nombre', type: TYPES.VarChar, value: nombre},
+    ];
+    return execQuery.execWriteCommand(query, parameters);
+};
+
+const updateCentroDeportivo = (centroDeportivoData) => {
+    const {
+        id_centro_deportivo,
+        nombre,
+    } = centroDeportivoData;
+    const query = `
+        UPDATE [dbo].[centro_deportivo]
+        SET nombre=@nombre
+        WHERE id_centro_deportivo = @id_centro_deportivo
+    `;
+    const parameters = [
+        {name: 'id_centro_deportivo', type: TYPES.Int, value: id_centro_deportivo},
+        {name: 'nombre', type: TYPES.VarChar, value: nombre},
+    ];
+    return execQuery.execWriteCommand(query, parameters);
+};
+
+
 module.exports = {
-    allTodo,
+    allCentroDeportivo,
+    deleteCentroDeportivo,
+    addCentroDeportivo,
+    updateCentroDeportivo,
 };
