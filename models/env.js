55890738c20047e378187a67f31b160efd1f3577
EduardoMaldonado287 <a00832361@itesm.mx>
2023-05-03 14:15:36 -0600

    temporal env config

---
+++ models/env.js
@@ -0,0 +1,10 @@
+API_PORT = 1433
+SERVER_TAG = "API SERVER LOCAL"
+
+DATABASE_SERVER = "gymcoders-server.database.windows.net"
+DATABASE_AUTH_TYPE = "default"
+DATABASE_USERNAME = "gymcoders_admin"
+DATABASE_PASSWORD = "Password_"
+DATABASE_NAME = "gymcoders-db"
+
+AZURE_STORAGE_CONNECTION_STRING = DefaultEndpointsProtocol=https;AccountName=gymcodersstorage;AccountKey=FFYKjLi7bSCs471uRwfLkir5+dwQf4cwdmfCV/iPXnho57dzIXuo2IRaiz8Ws7RhkeQ/KKVLCmG0+ASt6XN5Xw==;EndpointSuffix=core.windows.net
