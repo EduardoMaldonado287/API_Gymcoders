585be6f0b95b37622f0b3a3acd988ce8822577cf
EduardoMaldonado287 <a00832361@itesm.mx>
2023-05-23 18:20:31 -0600

    cambios en instalacion model

--- models/instalacion.model.js
+++ models/instalacion.model.js
@@ -5,11 +5,9 @@ const addInstalacion = (instalacionData) => {
     const {
         id_instalacion,
         id_centro_deportivo,
-        id_horario,
         nombre,
         id_deporte,
         imagen,
-        esta_habilitada,
         hora_inicio_es,
         hora_final_es,
         hora_inicio_fds,
@@ -17,18 +15,16 @@ const addInstalacion = (instalacionData) => {
     } = instalacionData;
     const query = `
         INSERT INTO [dbo].[instalacion] (id_instalacion, id_centro_deportivo, id_intervalo, 
-            id_horario, nombre, id_deporte, imagen, esta_habilitada, hora_inicio_es, hora_inicio_fds, hora_final_fds)
-        VALUES (@id_instalacion, @id_centro_deportivo, 1, @id_horario, @nombre, 
-            @id_deporte, @imagen, @esta_habilitada, @hora_inicio_es, @hora_final_es, @hora_inicio_fds, @hora_final_fds)
+            nombre, id_deporte, imagen, hora_inicio_es, hora_inicio_fds, hora_final_fds)
+        VALUES (@id_instalacion, @id_centro_deportivo, 1, @nombre, 
+            @id_deporte, @imagen, @hora_inicio_es, @hora_final_es, @hora_inicio_fds, @hora_final_fds)
     `;
     const parameters = [
         { name: 'id_instalacion', type: TYPES.Int, value: id_instalacion },
         { name: 'id_centro_deportivo', type: TYPES.Int, value: id_centro_deportivo },
-        { name: 'id_horario', type: TYPES.Int, value: id_horario },
         { name: 'nombre', type: TYPES.VarChar, value: nombre },
         { name: 'id_deporte', type: TYPES.Int, value: id_deporte },
         { name: 'imagen', type: TYPES.VarChar, value: imagen },
-        { name: 'esta_habilitada', type: TYPES.Bit, value: esta_habilitada },
         { name: 'hora_inicio_es', type: TYPES.VarChar, value: hora_inicio_es },
         { name: 'hora_final_es', type: TYPES.VarChar, value: hora_final_es },
         { name: 'hora_inicio_fds', type: TYPES.VarChar, value: hora_inicio_fds },
